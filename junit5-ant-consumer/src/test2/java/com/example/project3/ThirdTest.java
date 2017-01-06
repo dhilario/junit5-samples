a5352c25a15194189575671b9432c263ef2b63da
Dann Ryan Hilario <dannhilario@Danns-MacBook-Pro.local>
2017-01-06 17:46:36 +0800

    Need to transfer laptop

---
+++ junit5-ant-consumer/src/test2/java/com/example/project3/ThirdTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package com.example.project3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("fast")
+class ThirdTest {
+
+	@Test
+	void myThirdTest() {
+		Calculator calculator = new Calculator();
+		assertEquals(4, calculator.add(2, 2), "2 + 2 should equal 4");
+	}
+
+}
